e25d95169be0d8278d0fcb146f111ee484af0a86
luanh-s <luanhs@ucl.br[filter lfs]>
2017-12-02 21:18:51 -0200

    TESTE
    TESTE

--- BACKEND.js
+++ BACKEND.js
@@ -900,7 +900,8 @@ function DesenhaProblemaProposto (){
   			}
   		}
 		  ///////////////////////////////////// NESSA FUNÇÃO, SAI A MATRIZ DE FORÇAS APLICADAS //////////////////
-	  }
+      console.log("ArrayForcasEMomentosAplicados FINAL [E    ]" ArrayForcasEMomentosAplicados)
+    }
 	///////////////////////////////// ESSAS 3 FUNÇÕES GERAM O VETOR COM AS FORÇAS E MOMENTOS APLICADOS NOS NÓS
 
 
